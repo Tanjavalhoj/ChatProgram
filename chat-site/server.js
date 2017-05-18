d69d6e4690973b06ac8812efadb9665e23e07dd9
FrederikGP <f.gp@live.dk>
2017-05-18 13:17:24 +0200

    update
    kan nu skrive og modtage beskeder

--- chat-site/server.js
+++ chat-site/server.js
@@ -12,3 +12,28 @@ console.log('server is running....');
 app.get('/', function(reg,res){
 res.sendFile(__dirname + '/index.html');
 });
+
+io.sockets.on('connection',function(socket){
+
+connections.push(socket);
+console.log('connected: %s sockets connected', connections.length);
+
+
+
+
+  //Disconnect
+socket.on('disconnect', function(data){
+
+  connections.splice(connections.indexOf(socket), 1);
+  console.log('Disconnected: %s sockets connected ', connections.length );
+
+  });
+
+
+  // Send message
+  socket.on('send message', function(data){
+    console.log(data);
+    io.sockets.emit('new message',{msg: data});
+  });
+
+});
